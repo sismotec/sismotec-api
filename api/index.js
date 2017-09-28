bb794016b9a19a8f2818fcb8a2ad715d366fcdb2
Fernando Lopez Martinez <fer.lopez94@outlook.com>
2017-09-28 16:53:40 -0500

    Avanzar GET /beneficiarios/necesidades

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const api = express.Router();
 
-const beneficiaries = require('./beneficiaries');
+const beneficiarios = require('./beneficiarios');
 const ordenes_envio = require('./orden_envio');
 const necessities = require('./necessities');
 const recursos = require('./recursos');
 
 api.get('/', (req, res) => res.send({message: 'Hello World! This is SismoTec API!'}));
-api.use('/beneficiaries', beneficiaries);
+api.use('/beneficiarios', beneficiarios);
 api.use('/recursos', recursos);
 api.use('/necessities', necessities);
 api.use('/ordenes-envio', ordenes_envio);
--- api/orden_envio/post/index.js
+++ api/orden_envio/post/index.js
@@ -5,23 +5,24 @@ const NecesidadBeneficiario = models.NecesidadBeneficiario;
 
 
 let handler = (req, res) => {
-  id_centro_acopio = req.body.id_centro_acopio
-  id_necesidad_beneficiario = req.body.id_necesidad_beneficiario
-  recursos = req.body.recursos
-  CentroDeAcopio.findById(id_centro_acopio)
-    .then(ca => {
-      if(ca){
-        NecesidadBeneficiario.findById(id_necesidad_beneficiario)
-        .then(necesidad_b => {
-          recursos_orden = []
-          recursos.forEach(recurso => {
-            aux = 
-          });
-          oe = OrdenEnvio.build({})
-        });
-        res.send({centro: ca});
-      }
-    });
+  res.send();
+  // id_centro_acopio = req.body.id_centro_acopio
+  // id_necesidad_beneficiario = req.body.id_necesidad_beneficiario
+  // recursos = req.body.recursos
+  // CentroDeAcopio.findById(id_centro_acopio)
+  //   .then(ca => {
+  //     if(ca){
+  //       NecesidadBeneficiario.findById(id_necesidad_beneficiario)
+  //       .then(necesidad_b => {
+  //         recursos_orden = []
+  //         recursos.forEach(recurso => {
+  //           aux = 
+  //         });
+  //         oe = OrdenEnvio.build({})
+  //       });
+  //       res.send({centro: ca});
+  //     }
+  //   });
 };
 
 module.exports = {handler};
