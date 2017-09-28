b51876d76488fbf62648d79ddbd9418365e8d5c4
Santiago Sandoval <santiago@kinedu.com>
2017-09-28 12:02:51 -0500

    ordenes-envio

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = express.Router();
 
 const beneficiaries = require('./beneficiaries');
-const orden_envio = require('./orden_envio');
+const ordenes_envio = require('./orden_envio');
 const necessities = require('./necessities');
 const recursos = require('./recursos');
 
@@ -10,6 +10,6 @@ api.get('/', (req, res) => res.send({message: 'Hello World! This is SismoTec API
 api.use('/beneficiaries', beneficiaries);
 api.use('/recursos', recursos);
 api.use('/necessities', necessities);
-api.use('/orden_envio', orden_envio);
+api.use('/ordenes-envio', ordenes_envio);
 
 module.exports = api;
