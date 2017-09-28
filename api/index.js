1cff242f8578146c27e68454f9b052d8af299d5a
Santiago Sandoval <santiago@kinedu.com>
2017-09-28 17:05:55 -0500

    Changed routes names

--- api/index.js
+++ api/index.js
@@ -3,13 +3,13 @@ const api = express.Router();
 
 const beneficiarios = require('./beneficiarios');
 const ordenes_envio = require('./orden_envio');
-const necessities = require('./necessities');
+const necesidades = require('./necesidades');
 const recursos = require('./recursos');
 
 api.get('/', (req, res) => res.send({message: 'Hello World! This is SismoTec API!'}));
 api.use('/beneficiarios', beneficiarios);
 api.use('/recursos', recursos);
-api.use('/necessities', necessities);
+api.use('/necesidades', necesidades);
 api.use('/ordenes-envio', ordenes_envio);
 
 module.exports = api;
