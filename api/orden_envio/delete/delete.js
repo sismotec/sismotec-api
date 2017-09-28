5a1b5fedcf0dab2874289fb72ca33539a66483ea
jorgerubio96 <jorge.rubiobarboza96@gmail.com>
2017-09-28 12:23:12 -0500

    changes to delte file

--- api/orden_envio/delete/delete.js
+++ api/orden_envio/delete/delete.js
@@ -1,29 +1,28 @@
-// var request = require('request');
 const models = require('../../../models');
-const OrdenEnvio = models.OrdenEnvio;
-const CentroDeAcopio = models.CentroDeAcopio;
-const NecesidadBeneficiario = models.NecesidadBeneficiario;
+const {OrdenEnvio, CentroDeAcopio, NecesidadBeneficiario, RecursoOrden} = models;
 
+OrdenEnvio.findAll({
+	attributes: ['id']
+});
 
 let handler = (req, res) => {
+   OrdenEnvio.findAll({
+   	attributes: ['id']
+   });
+
+   RecursoOrden.findAll({
+   	attributes: ['id']
+   });
+
+   
+
    CentroDeAcopio.findById(1)
    .then(ca => {
      res.send({centro: ca});
    });
 };
 
+
 module.exports = {handler};
 
 
-// request({
-//   method: 'DELETE',
-//   url: 'https://private-anon-7e05af3de2-sismotecapi.apiary-mock.com/centros-acopio/ordenes-envio',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: "{  \"orderID\": 5}"
-// }, function (error, response, body) {
-//   console.log('Status:', response.statusCode);
-//   console.log('Headers:', JSON.stringify(response.headers));
-//   console.log('Response:', body);
-// });
