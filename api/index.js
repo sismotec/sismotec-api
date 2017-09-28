8d8155f64ffcc0a76e900492db7968bc5088a888
Santiago Sandoval <santiago@kinedu.com>
2017-09-28 10:49:24 -0500

    Started centro-acopio (necessities)

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const api = express.Router();
 
 const beneficiaries = require('./beneficiaries');
+const centrosAcopio = require('./centros-acopio');
 
 api.get('/', (req, res) => res.send({message: 'Hello World! This is SismoTec API!'}));
 api.use('/beneficiaries', beneficiaries);
+api.use('/centros-acopio', centrosAcopio);
 
 module.exports = api;
