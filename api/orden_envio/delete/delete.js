8163b2a4f0efd4b2dcdfe450ca5240cc0814b026
jorgerubio96 <jorge.rubiobarboza96@gmail.com>
2017-09-28 17:38:43 -0500

    Updates to delete file

--- api/orden_envio/delete/delete.js
+++ api/orden_envio/delete/delete.js
@@ -8,13 +8,18 @@ let handler = (req, res) => {
 	if(id_orden == null){
 		res.send({code: 404, message: 'Order not found'});	
 	}else{
-		OrdenEnvio.findById(id_orden).then(orden => {
-			res.send({id_orden: orden});
-		});
+		// RecursoOrden.destroy({
+		//   where: {
+		//     id_orden: id_orden_recurso,
+		//   }
+		// });
 		OrdenEnvio.findById(id_orden).then(order => { 
 			order.update({
 				status: 'cancelado',
 			});
+		});
+		OrdenEnvio.findById(id_orden).then(orden => {
+			res.send({id_orden: orden});
 		});
 	}
 };
