9c37762d194487278b8292eb8834ba8194674709
jorgerubio96 <jorge.rubiobarboza96@gmail.com>
2017-09-29 09:47:36 -0500

    Register users

--- api/auth/register.js
+++ api/auth/register.js
@@ -5,15 +5,76 @@ var shajs = require('sha.js')
 let handler = (req, res) => {
 	id_usuario = req.body.id
 	tipo_usuario = req.body.tipo
+	nombre_organizacion = req.body.nombre_organizacion
+	nombre_responsable = req.body.nombre_responsable
+	email = req.body.email
+	celular = req.body.celular
+	latitud = req.body.latitud
+	longitud = req.body.longitud
+	password = req.body.password
+	estado = req.body.estado
+
+	password = sha256(password);
 
 	if(id_usuario == null && tipo_usuario
 		== null){
 		res.send({code: 404, message: 'Register Error'});	
 	}else{
 		if (tipo_usuario == "Centro de acopio") {
-			CentroDeAcopio
+			CentroDeAcopio.create({ 
+				id: id_usuario,
+				latitud: latitud,
+				longitud: longitud,
+				nombre_organizacion: nombre_organizacion,
+				nombre_responsable: nombre_responsable,
+				celular: celular,
+				email: email,
+				estado: estado,
+				password: password })
+			  .then(() => CentroDeAcopio.findOrCreate({
+			  	where: {
+			  		email: email,
+			  		celular: celular,
+			  		id: id_usuario
+			  	}, 
+			  	defaults: {
+			  		horario: null
+			  	}}))
+			  .spread((user, created) => {
+			    console.log(user.get({
+			      plain: true
+			    }))
+			    console.log(created)
+				})
+			  .catch(function (err) {
+			  	res.send({code: 404, message: 'Register Error'});
+			  });
 		}else{
-			Beneficiario
+			Beneficiario.create({ 
+				id: id_usuario,
+				latitud: latitud,
+				longitud: longitud,
+				nombre_instituto: nombre_organizacion,
+				nombre_responsable: nombre_responsable,
+				celular: celular,
+				email: email,
+				estado: estado,
+				password: password })
+			  .then(() => CentroDeAcopio.findOrCreate({
+			  	where: {
+			  		email: email,
+			  		celular: celular,
+			  		id: id_usuario
+			  	}}))
+			  .spread((user, created) => {
+			    console.log(user.get({
+			      plain: true
+			    }))
+			    console.log(created)
+				})
+			  .catch(function (err) {
+			  	res.send({code: 404, message: 'Register Error'});
+			  });
 		}
 	}
 };
