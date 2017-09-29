8f63ff04e23078323d0d714f4667787435afc1b6
Axel Suarez <axel.srz@gmail.com>
2017-09-28 20:11:46 -0500

    update on sequelize relations

---
+++ api/beneficiarios/necesidades/post/index.js
@@ -0,0 +1,10 @@
+const models = require('../../../models');
+const { NecesidadBeneficiario, CantidadBeneficiarioRecurso, sequelize} = models;
+
+
+let handler = (req, res) => {
+  const {id_beneficiario, recursos} = req.body;
+  nb = NecesidadBeneficiario
+};
+
+module.exports = {handler};
